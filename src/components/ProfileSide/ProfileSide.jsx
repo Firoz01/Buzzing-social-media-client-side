b6a64e36669f4df5d08a0699e8cd888e94610bb5
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-06-26 12:28:00 +0600

    solve the vl issue and complete profile card

--- src/components/ProfileSide/ProfileSide.jsx
+++ src/components/ProfileSide/ProfileSide.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LogoSearch from '../LogoSearch/LogoSearch';
+import ProfileCard from '../ProfileCard/ProfileCard';
 import './ProfileSide.css'
 
 const ProfileSide = () => {
     return (
         <div>
             <div className="ProfileSide">
-                <LogoSearch/>
+                <LogoSearch />
+                <ProfileCard/>
             </div>
         </div>
     );
