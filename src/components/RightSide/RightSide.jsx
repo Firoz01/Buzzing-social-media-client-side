f58f70235434671e959c801aa40d7d54d51518cf
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-06-30 15:05:57 +0600

    complete share model component

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,17 +1,25 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar/Navbar';
+import ShareModal from '../ShareModal/ShareModal';
 import TrendCard from '../TrendCard/TrendCard';
 import './RightSide.css'
 
 
 const RightSide = () => {
+     const [modalOpened, setModalOpened] = useState(false);
     return (
-        <div className='RightSide'>
-            <Navbar />
-            <TrendCard />
-            <button className='button r-button'>Share</button>
-        </div>
+      <div className="RightSide">
+        <Navbar />
+        <TrendCard />
+        <button
+          className="button r-button"
+          onClick={() => setModalOpened(true)}
+        >
+          Share
+        </button>
+        <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
+      </div>
     );
 };
 
