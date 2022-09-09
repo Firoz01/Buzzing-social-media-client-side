9d4a1c48ecb9bd8edd59c4aaf919796b2a5f2300
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-09-09 10:44:56 +0600

    like and unlike working fine

--- src/Api/PostRequest.js
+++ src/Api/PostRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { url } from "./constant";
 
-
 const API = axios.create({ baseURL: url });
 
 export const getTimeLinePosts = (id) => API.get(`/api/v1/posts/${id}/timeline`);
+
+export const likePost = (id, userId) =>
+  API.put(`/api/v1/posts/${id}/like`, { userId: userId });
