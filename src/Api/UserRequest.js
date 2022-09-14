f15a915c5228e8deed4a2e7491342121ff1a49e1
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-09-14 23:24:53 +0600

    jwt authorization

--- src/Api/UserRequest.js
+++ src/Api/UserRequest.js
@@ -4,6 +4,15 @@ import { url } from "./constant";
 
 const API = axios.create({ baseURL: url });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+  return req;
+});
+
 export const getUser = (userId) => API.get(`/api/v1/user/${userId}`);
 
 export const updateUser = (id, formData) =>
